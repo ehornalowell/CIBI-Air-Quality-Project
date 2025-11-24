2a92dd539050aa1f6843403dd551c28c1ae8ab21
ehornalowell <ehornalowell@sdnhm.org>
2025-11-24 10:07:06 -0800

    Add CIBI and Air Quality Project script

---
+++ bigpub_stats_nov2025.R
@@ -0,0 +1,30 @@
+########################  set working environment ################################
+
+setwd("./CIBI BOLD ANALYSIS/BIGPUB")
+
+rm(list = ls()) # clear working environment
+
+
+############################################################################################
+############################## load packages ###########################################
+
+library(dplyr)
+
+
+
+#########################################################################################
+############################### read in data ################################################
+
+####### 1. Load BOLD data 
+
+all_sdnhm <- read.csv("BOLD_SDNHM_all_final_Nov2025.csv")
+
+#filter Exact.site for observatory sites (5)
+sdnhm_obs <- filter(all_sdnhm, Exact_site %in% c("Tierra_Del_Sol_SDAA", "Lopez_Ridge_Vernal_Pools", "Picacho_State_Park", "Wheatley_Ranch", "Anza_Borrego_UC_Reserve"))
+
+#filter Sampling_protocol for only malaise trap
+sdnhm_obs_mal <- filter(sdnhm_obs, Sampling_protocol == "Malaise_Trap")
+
+## calculate abundance
+
+####### 2. Load climate data 
