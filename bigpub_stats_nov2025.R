ce2365b28e5a18ceddc9fe0af85d80faaf5f6619
ehornalowell <ehornalowell@sdnhm.org>
2025-11-25 11:16:01 -0800

    cleaning BOLD BIN data

--- bigpub_stats_nov2025.R
+++ bigpub_stats_nov2025.R
@@ -22,15 +22,35 @@ all_sdnhm <- read.csv("https://raw.githubusercontent.com/ehornalowell/CIBI-Air-Q
 
 ####### 2. Clean and subset BOLD data 
 
-#filter Exact.site for observatory sites (5) and filter Sampling.protocol for malaise trap only
+# 2a. filter Exact.site for observatory sites (5) and filter Sampling.protocol for malaise trap only
 
 sdnhm_obs_mal <- all_sdnhm %>%
   filter(
-    Exact.Site %in% c("Tierra_Del_Sol_SDAA", "Lopez_Ridge_Vernal_Pools", "Picacho_State_Park", "Wheatley_Ranch", "Anza_Borrego_UC_Reserve"),
-    Sampling.Protocol == "Malaise trap")
+    Exact.Site %in% c("Tierra Del Sol SDAA", "Lopez Ridge Vernal Pools", "Picacho State Park", "Wheatley Ranch", "Anza Borrego UC Reserve"),
+    Sampling.Protocol %in% c("Malaise Trap", "Malaise trap"))
 
-#filter Sampling_protocol for only malaise trap
-#sdnhm_obs_mal <- filter(sdnhm_obs, Sampling_protocol == "Malaise_Trap")
+# 2b. make sure i have november 2023 data for PSP and ABUCR since that was missing originally
+unique_sites_dates <- sdnhm_obs_mal %>%
+  distinct(Exact.Site, Collection.Date.y) ## all pairs are there! yay! 
+
+# 2c. create dataframe that removes all rows without assigned BIN
+sdnhm_noNABINs <- sdnhm_obs_mal %>%
+  filter(!is.na(BIN)) 
+  
+  #number of rows with BIN = NA
+  sum(is.na(sdnhm_obs_mal$BIN)) #7505 specimens ~8.5%
+  
+# 2d. Using collection.date.y column, create a new column that only has month and year
+  sdnhm_month.year <- sdnhm_obs_mal %>%
+    mutate(
+      Month_Year = format(as.Date(Collection.Date.y, format = "%d-%b-%y"), "%b-%y")
+    )
+    
+    #do same for dataframe with no NA bins
+    no.NA.BINs_month.year <- sdnhm_noNABINs %>%
+      mutate(
+        Month_Year = format(as.Date(Collection.Date.y, format = "%d-%b-%y", "%b-%y"))
+      )
 
 ## calculate abundance
 
