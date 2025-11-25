e14c89e3e10e438e2e689deea57de83003eb93e0
ehornalowell <ehornalowell@sdnhm.org>
2025-11-25 11:59:13 -0800

    cleaning BOLD BIN data

--- bigpub_stats_nov2025.R
+++ bigpub_stats_nov2025.R
@@ -20,6 +20,7 @@ all_sdnhm <- read.csv("https://raw.githubusercontent.com/ehornalowell/CIBI-Air-Q
 #csv is from another r script. file was too large to upload to Github repo. so I filtered out for all SDNHM data using collectors. 
 #That is what this csv file is.
 
+
 ####### 2. Clean and subset BOLD data 
 
 # 2a. filter Exact.site for observatory sites (5) and filter Sampling.protocol for malaise trap only
@@ -29,18 +30,23 @@ sdnhm_obs_mal <- all_sdnhm %>%
     Exact.Site %in% c("Tierra Del Sol SDAA", "Lopez Ridge Vernal Pools", "Picacho State Park", "Wheatley Ranch", "Anza Borrego UC Reserve"),
     Sampling.Protocol %in% c("Malaise Trap", "Malaise trap"))
 
-# 2b. make sure i have november 2023 data for PSP and ABUCR since that was missing originally
+# 2b. Mutate "Malaise trap" to match "Malaise Trap" so only one unique value in sampling.protocol column
+sdnhm_obs_mal <- sdnhm_obs_mal %>%
+  mutate(Sampling.Protocol = recode(Sampling.Protocol,
+                                    "Malaise trap" = "Malaise Trap"))
+
+# 2c. make sure i have november 2023 data for PSP and ABUCR since that was missing originally
 unique_sites_dates <- sdnhm_obs_mal %>%
   distinct(Exact.Site, Collection.Date.y) ## all pairs are there! yay! 
 
-# 2c. create dataframe that removes all rows without assigned BIN
+# 2d. create dataframe that removes all rows without assigned BIN
 sdnhm_noNABINs <- sdnhm_obs_mal %>%
   filter(!is.na(BIN)) 
   
   #number of rows with BIN = NA
   sum(is.na(sdnhm_obs_mal$BIN)) #7505 specimens ~8.5%
   
-# 2d. Using collection.date.y column, create a new column that only has month and year
+# 2e. Using collection.date.y column, create a new column that only has month and year
   sdnhm_month.year <- sdnhm_obs_mal %>%
     mutate(
       Month_Year = format(as.Date(Collection.Date.y, format = "%d-%b-%y"), "%b-%y")
@@ -49,9 +55,22 @@ sdnhm_noNABINs <- sdnhm_obs_mal %>%
     #do same for dataframe with no NA bins
     no.NA.BINs_month.year <- sdnhm_noNABINs %>%
       mutate(
-        Month_Year = format(as.Date(Collection.Date.y, format = "%d-%b-%y", "%b-%y"))
+        Month_Year = format(as.Date(Collection.Date.y, format = "%d-%b-%y"), "%b-%y")
       )
 
-## calculate abundance
+# 2f. Remove unnecessary columns - only keep ones for downstream analysis 
+    clean_sdnhm <- sdnhm_month.year %>%
+      select(-c(Project.Code, Identifier, Collectors, Collection.Date.y, Elev, Collection.Date.Accuracy, Habitat, Sampling.Protocol))
+      
+    # do the same for dataframe without values BIN = NA
+    clean_sdnhm_noNABIN <- no.NA.BINs_month.year %>%
+      select(-c(Project.Code, Identifier, Collectors, Collection.Date.y, Elev, Collection.Date.Accuracy, Habitat, Sampling.Protocol))
 
-####### 2. Load climate data 
+
+####### 3. Quick summary stats 
+    ## total number of specimens sequenced at five observatory sites, malaise traps: 94,122
+    
+    ## number of specimens not assigned a BIN : 7505
+    
+    ## number of specimens ID to Fam, Subfam, Genus, Species
+    
