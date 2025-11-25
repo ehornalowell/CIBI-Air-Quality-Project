5be4401175c4806dd7548fd85be56c6a44b68cf9
ehornalowell <ehornalowell@sdnhm.org>
2025-11-24 16:26:28 -0800

    # out lines of code that aren't fully fixed yet

--- bigpub_stats_nov2025.R
+++ bigpub_stats_nov2025.R
@@ -19,10 +19,10 @@ library(dplyr)
 all_sdnhm <- read.csv("https://raw.githubusercontent.com/ehornalowell/CIBI-Air-Quality-Project/main/sdnhm_COIdata.csv")
 
 #filter Exact.site for observatory sites (5)
-sdnhm_obs <- filter(all_sdnhm, Exact_site %in% c("Tierra_Del_Sol_SDAA", "Lopez_Ridge_Vernal_Pools", "Picacho_State_Park", "Wheatley_Ranch", "Anza_Borrego_UC_Reserve"))
+#sdnhm_obs <- filter(all_sdnhm, Exact_site %in% c("Tierra_Del_Sol_SDAA", "Lopez_Ridge_Vernal_Pools", "Picacho_State_Park", "Wheatley_Ranch", "Anza_Borrego_UC_Reserve"))
 
 #filter Sampling_protocol for only malaise trap
-sdnhm_obs_mal <- filter(sdnhm_obs, Sampling_protocol == "Malaise_Trap")
+#sdnhm_obs_mal <- filter(sdnhm_obs, Sampling_protocol == "Malaise_Trap")
 
 ## calculate abundance
 
