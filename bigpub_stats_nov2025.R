1a4e1860db0cf308c8b9651a2d6571792b9ba628
ehornalowell <ehornalowell@sdnhm.org>
2025-11-25 12:58:16 -0800

    adding summary stats and figs

--- bigpub_stats_nov2025.R
+++ bigpub_stats_nov2025.R
@@ -8,7 +8,8 @@ rm(list = ls()) # clear working environment
 ############################## load packages ###########################################
 
 library(dplyr)
-
+library(ggplot2)
+library(tidyr)
 
 
 #########################################################################################
@@ -66,11 +67,92 @@ sdnhm_noNABINs <- sdnhm_obs_mal %>%
     clean_sdnhm_noNABIN <- no.NA.BINs_month.year %>%
       select(-c(Project.Code, Identifier, Collectors, Collection.Date.y, Elev, Collection.Date.Accuracy, Habitat, Sampling.Protocol))
 
+    
+    
+
+##########################################################################################################################
+####################### summary stats & diversity calculations ###########################################################
 
 ####### 3. Quick summary stats 
-    ## total number of specimens sequenced at five observatory sites, malaise traps: 94,122
+# 3a. total number of specimens sequenced at five observatory sites, malaise traps: 94,122
     
-    ## number of specimens not assigned a BIN : 7505
+# 3b. number of specimens not assigned a BIN : 7505
     
-    ## number of specimens ID to Fam, Subfam, Genus, Species
+# 3c. number of specimens ID to Fam, Subfam, Genus, Species -- bar graph
+    id.stats <- clean_sdnhm_noNABIN %>%
+      summarise(across(everything(), ~sum(!is.na(.) & . !="")))
+          ## specimens ID to Order = ALL 86617
+          ## specimens ID to Family = 84477
+          ## specimens ID to Subfamily = 39871
+          ## specimens ID to Genus = 35892
+          ## specimens ID to Species = 15798
+    
+    # make df long for fig
+    id.stats.long <- id.stats %>%
+      pivot_longer(
+        cols = everything(),
+        names_to = "Taxonomic_Level",
+        values_to = "Specimen_Count"
+      )
+    
+    #subset specific rows 
+    id.stats.long.filtered <- id.stats.long %>%
+      filter(Taxonomic_Level %in% c("Order", "Family", "Subfamily", "Genus", "Species"))
+    
+    #reorder rows so figure is in taxonomic order - highest to lowest level
+    id.stats.long.filtered$Taxonomic_Level <- factor(
+      id.stats.long.filtered$Taxonomic_Level,
+      levels = c("Order", "Family", "Subfamily", "Genus", "Species")
+    )
+    
+    #simple bar graph
+    ggplot(id.stats.long.filtered, aes(x = Taxonomic_Level, y = Specimen_Count)) +
+      geom_col() +
+      theme_classic()
+    
+# 3d. number of specimens ID to level vs. not identified to that level. -- figure 
+    # calculate total # of specimens in your dataset
+    total_n <- nrow(clean_sdnhm_noNABIN)
+    
+    # create vector of taxonomy columns im about to evaluate
+    tax_levels <- c("Order", "Family", "Subfamily", "Genus", "Species")
+    
+    # create new table
+    tax_summary <- tibble(
+      Taxonomic_Level = tax_levels, # first column will be taxonomic levels
+      Identified = sapply( #apply same calculation to each element of tax_apply (aka each column)
+        tax_levels,
+        function(col) sum(!is.na(clean_sdnhm_noNABIN[[col]]) & clean_sdnhm_noNABIN[[col]] != "") # calculate how many specimens were identified at each level
+      )
+    ) %>%
+      mutate(
+        Total = total_n,
+        Not_Identified = Total - Identified ##building two more columns in dataframe 
+      ) %>%
+      pivot_longer( #pivot from wide to long format to be able to plot
+        cols = c(Identified, Not_Identified),
+        names_to = "Status",
+        values_to = "Count"
+      )
+    
+    #order taxonomic levels properly for xaxis order of columns
+    tax_summary <- tax_summary %>%
+      mutate(
+        Taxonomic_Level = factor(
+          Taxonomic_Level,
+          levels = c("Order", "Family", "Subfamily", "Genus", "Species")
+        )
+      )
+    
+    #plot figure! one bar per taxonomic level, with identified vs. not identified. stacked
+    ggplot(tax_summary, aes(x = Taxonomic_Level, y = Count, fill = Status)) +
+      geom_col() +
+      theme_classic() +
+      labs(
+        x = "Taxonomic Level",
+        y = "Number of Specimens",
+        fill = ""
+      )
+
+############    
     
