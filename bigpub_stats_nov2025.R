3b4a903d2d74501c1272ea3863f5c8d0e1a3228d
ehornalowell <ehornalowell@sdnhm.org>
2025-11-24 17:03:19 -0800

    subsetting data

--- bigpub_stats_nov2025.R
+++ bigpub_stats_nov2025.R
@@ -1,4 +1,3 @@
-## testing 123
 ########################  set working environment ################################
 
 
@@ -17,10 +16,18 @@ library(dplyr)
 
 ####### 1. Load BOLD data 
 
-all_sdnhm <- read.csv("https://raw.githubusercontent.com/ehornalowell/CIBI-Air-Quality-Project/main/sdnhm_COIdata.csv")
+all_sdnhm <- read.csv("https://raw.githubusercontent.com/ehornalowell/CIBI-Air-Quality-Project/main/sdnhm_COIdata.csv") 
+#csv is from another r script. file was too large to upload to Github repo. so I filtered out for all SDNHM data using collectors. 
+#That is what this csv file is.
 
-#filter Exact.site for observatory sites (5)
-#sdnhm_obs <- filter(all_sdnhm, Exact_site %in% c("Tierra_Del_Sol_SDAA", "Lopez_Ridge_Vernal_Pools", "Picacho_State_Park", "Wheatley_Ranch", "Anza_Borrego_UC_Reserve"))
+####### 2. Clean and subset BOLD data 
+
+#filter Exact.site for observatory sites (5) and filter Sampling.protocol for malaise trap only
+
+sdnhm_obs_mal <- all_sdnhm %>%
+  filter(
+    Exact.Site %in% c("Tierra_Del_Sol_SDAA", "Lopez_Ridge_Vernal_Pools", "Picacho_State_Park", "Wheatley_Ranch", "Anza_Borrego_UC_Reserve"),
+    Sampling.Protocol == "Malaise trap")
 
 #filter Sampling_protocol for only malaise trap
 #sdnhm_obs_mal <- filter(sdnhm_obs, Sampling_protocol == "Malaise_Trap")
